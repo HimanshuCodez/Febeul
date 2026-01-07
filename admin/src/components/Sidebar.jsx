41286aafc87036a02c140557e71f23058c300e52
Himanshu gaur <himanshugaur055@gmail.com>
2026-01-07 23:01:54 +0530

    changes

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -22,10 +22,10 @@ const Sidebar = () => {
                 <p className='hidden md:block'>Orders</p>
             </NavLink>
 
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/allusers">
+            {/* <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/allusers">
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
                 <p className='hidden md:block'>All Users</p>
-            </NavLink>
+            </NavLink> */}
 
         </div>
 
