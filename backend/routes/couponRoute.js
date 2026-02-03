09d8d7b2423a843db6043d6cb05535b4cda8aa78
Himanshu gaur <himanshugaur055@gmail.com>
2026-02-03 23:26:18 +0530

    coupon fixed

--- backend/routes/couponRoute.js
+++ backend/routes/couponRoute.js
@@ -2,13 +2,14 @@ import express from 'express';
 import { addCoupon, listCoupons, removeCoupon, applyCoupon, getActiveCoupons } from '../controllers/couponController.js';
 import auth from '../middleware/auth.js';
 import adminAuth from '../middleware/adminAuth.js';
+import authUser from '../middleware/auth.js';
 
 const couponRouter = express.Router();
 
 // Admin Routes
-couponRouter.post('/add', adminAuth, addCoupon);
-couponRouter.get('/list', adminAuth, listCoupons);
-couponRouter.post('/remove', adminAuth, removeCoupon);
+couponRouter.post('/add', authUser, addCoupon);
+couponRouter.get('/list', authUser, listCoupons);
+couponRouter.post('/remove', authUser, removeCoupon);
 
 // User Routes
 couponRouter.post('/apply', auth, applyCoupon);
