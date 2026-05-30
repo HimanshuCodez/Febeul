560d8437307f2557f75fd34d3388725e40965f65
Himanshu gaur <himanshugaur055@gmail.com>
2026-05-30 22:25:37 +0530

    changes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay, getRazorpayKey, generateInvoice, getOrderById} from '../controllers/orderController.js'
+import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay, getRazorpayKey, generateInvoice, getOrderById, cancelOrder} from '../controllers/orderController.js'
 import { requestRefund, updateShiprocketStatusWebhook } from '../controllers/refundController.js'; // Import new refund functions
 import adminAuth  from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
